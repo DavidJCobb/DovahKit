bf0c6b46577fa1d4fbb7e801cfea35e7036eab72
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2019-10-16 00:39:21 +0200

    cobb::bitset documentation comments

--- SkyEditSln/SkyEditSln/SkyEdit/helpers/bitset.h
+++ SkyEditSln/SkyEditSln/SkyEdit/helpers/bitset.h
@@ -9,10 +9,12 @@ namespace cobb {
          static constexpr uint32_t all_bits_set      = std::numeric_limits<uint32_t>::max();
          static constexpr int      chunk_count       = count / bits_per_chunk + (count % bits_per_chunk ? 1 : 0);
          //
-         static constexpr int      undershoot_cc     = count / bits_per_chunk;
+         // To understand these next constexprs, see the comments for find_first_clear.
+         //
+         static constexpr int      undershoot_cc     = count / bits_per_chunk; // number of non-partial chunks
          static constexpr int      bits_in_partial   = count % bits_per_chunk;
          static constexpr bool     has_partial_chunk = bits_in_partial != 0;
-         static constexpr int      partial_chunk_max = (1 << (count % bits_per_chunk)) - 1;
+         static constexpr int      partial_chunk_max = (1 << (count % bits_per_chunk)) - 1; // like all_bits_set but for the partial chunk
          //
          uint32_t data[chunk_count];
          //
@@ -45,6 +47,11 @@ namespace cobb {
          }
          //
          int32_t find_first_clear() const {
+            //
+            // Finds the first zero bit in the set. This function performs significantly 
+            // better than looping from 0 to (count) and testing each individual bit, as you 
+            // would have to do when using std::bitset as of this writing.
+            //
             for (uint32_t i = 0; i < undershoot_cc; i++) {
                auto chunk = data[i];
                if (chunk != all_bits_set) {
@@ -56,6 +63,12 @@ namespace cobb {
                }
             }
             if (has_partial_chunk) {
+               //
+               // If the number of bits in the set isn't cleanly divisible by 32, then we're 
+               // going to have a final chunk that only uses some of its bits. We need to ONLY 
+               // LOOK AT THE BITS THAT THAT CHUNK ACTUALLY USES, or we'll end up returning bit 
+               // indices past the end of our set.
+               //
                auto chunk = data[chunk_count - 1];
                if (chunk != partial_chunk_max) {
                   for (uint8_t j = 0; j < bits_in_partial; j++) {
