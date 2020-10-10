f5b456512c92cb0e915f86eb5c2810fe32c221e4
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2020-10-10 17:45:52 +0200

    Plans: form ID compaction, ESL support, and similar
    I investigated and tested ESL files and have documented my findings. I now have to choose between two approaches: mimic the CK and the file format, and don't support ESL 0xFE mapping while editing; or go to the extra trouble to support that mapping while editing.
    
    The main pain point of supporting 0xFE is implementing the ability to mass renumber forms. However, we need to do that anyway in order to support form ID compaction, and being able to renumber single forms would improve our parity with xEdit. There are ways to make the process significantly less painful (by making the changes necessary to essentially have it and form deletion share boilerplate within loaded forms).

--- native/src/DovahKit/main.cpp
+++ native/src/DovahKit/main.cpp
@@ -81,35 +81,81 @@
 //
 //  - Support for Skyrim Special Edition.
 //
-//     - Bool on (file_load_order) indicating whether to handle SSE files or not, i.e. 
-//       whether to respect the ESL flag and whether to reserve slot 0xFE.
+//     = Within an ESL file, forms defined by the ESL file do not use the 0xFE prefix. 
+//       If an ESL has four masters (such that the last of them is prefixed 0x03), then 
+//       the ESL's own forms will use prefix 0x04. (xEdit will display the prefix as 
+//       0xFE; you must hex-edit the file to see the true prefix.)
 //
-//        - Needs to be accessible (read and write) to DovahKitCore, so that the editor 
-//          can handle localized_strings properly.
+//        = When the Creation Kit loads ESL files, it does not place them in slot 0xFE, 
+//          but rather gives them the same load order prefix as a non-ESL. This occurs 
+//          whether loading a single ESL or multiple.
 //
-//     - The backend should allow loading multiple ESLs; however, saving should fail 
-//       if any of the active file's masters would be an ESL.
+//     = A file can have ESLs as masters, and will number them the same as non-ESL 
+//       masters. If a file has one non-light master and one light master, then forms 
+//       from the light master will use load order prefix 0x01, and can be referenced 
+//       and overridden. An additional light master would use prefix 0x02.
 //
-//     - Form ID resolution needs to handle ESLs separately.
+//     = So there's two approaches we can take, then. The first is to allow the frontend 
+//       to decide whether (file_load_order) honors light plugins or just treats them as 
+//       non-light plugins; our particular frontend, being intended for editing, would 
+//       opt out of supporting light-plugins, just as the Creation Kit does.
 //
-//        - This in turn requires having ESLs in a separate list than (files), which 
-//          in turn requires that we basically audit every piece of code that uses 
-//          the (files) vector.
+//       The second approach is to make the backend support light plugins for all tasks, 
+//       and have it mass renumber the active file's form IDs when the active file gains 
+//       or loses "light" status during a save. If we do this, then we need a lot of 
+//       error checking and a lot of form ID fixup on save:
 //
-//     - When saving, form IDs need to be fixed up.
+//        - A save operation needs to fail if the active file would end up with too many 
+//          masters (non-light and light total).
 //
-//        - If the active file is a light plug-in, fix-up is required.
+//        - Light plug-in IDs need to be converted from 0xFE to master-list-relative; 
+//          the file format's own handling of form IDs doesn't support light plug-ins.
+//
+//        - When the active file's "light" flag changes, we need to mass renumber all of 
+//          its form IDs in memory, to change their load order prefix.
+//
+//       Mass renumbering is a particular pain point because that's even more boilerplate 
+//       that needs to be added to every loaded form class. However, we could potentially 
+//       combine it with the existing boilerplate for handling form deletion, if we allow 
+//       loaded forms to return a vector of pointers to their form_id_t members. (Note, 
+//       however, that there are form_id_t subclasses that override setter behavior, and 
+//       they do so entirely on type -- they're not virtual and they have no internal 
+//       members to disambiguate their types -- so we'd need multiple getters for each 
+//       form_id_t type, or we'd need to add type information to form_id_t.)
+//
+//       It's also worth noting that we need mass form renumbering anyway if we want to 
+//       offer the option to compact form IDs in preparation for saving an ESL, and that 
+//       being able to renumber single forms would give us some parity with xEdit.
+//
+//        - If we want to implement editing, then making form_id_t retain type information 
+//          (i.e. information indicating what use info flags should be applied as the ID 
+//          is changed) is the best approach. Then, we can modify the loaded form hooks -- 
+//          replace (Form::sever_outbound_references_to) with (Form::get_all_form_IDs) 
+//          which would return (std::vector<form_id_t*>). The code for deleting forms 
+//          could blindly loop over those IDs and call (form_id_t::set), while the code 
+//          for renumbering form IDs could directly overwrite them (in addition to 
+//          updating use info entries' keys, moving the form_stub within the file_load_order 
+//          form maps, and so on).
+//
+//        - Renumbering forms should send DovahKitCore signals:
+//
+//           - void onFormRenumberImminent(form_stub*, uint32_t oldID, uint32_t newID);
+//           - void onFormRenumberComplete(form_stub*, uint32_t oldID, uint32_t newID);
+//
+//          UI code needs to react responsibly to these.
+//
+//     = As such: when the backend is not being used for editing, it should be able to 
+//       load an arbitrary number of ESLs (subject to the limit on those) and properly 
+//       remap their form IDs during load. The question is whether we should support ESLs 
+//       during editing, given that the file format itself does not.
+//
+//     = Proper ESL form ID mapping would require storing ESL files in a separate list 
+//       from non-ESL files. This in turn requires that we audit every piece of code that 
+//       accesses the (file_load_order::files) vector.
 //
 //     - Option to save an Special active file as a Classic file and vice versa.
 //
 //     - Option to save an active ESL as a non-ESL, vice versa.
-//
-//     = NEEDED RESEARCH:
-//
-//        - Do forms created in an ESL use the 0xFE prefix, or do they use the same 
-//          prefix they would if created in a non-ESL file?
-//
-//        - How does the SSE CK display an active ESL's form IDs?
 //
 // THINGS TO LOOK INTO:
 //
