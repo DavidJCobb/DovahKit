6c15a030c5c8efb9264672a179f5f175e2f9b1d4
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2021-02-26 16:51:36 +0100

    Fix for prior commit
    I forgot to move the actual "meat" of the class out of the form stub file.

---
+++ native/src/DovahKit/dovah/form_stub_use_info_builder.cpp
@@ -0,0 +1,49 @@
+#include "form_stub_use_info_builder.h"
+
+namespace dovah {
+   form_stub_use_info_builder::form_stub_use_info_builder(form_stub& s) : _stub(s) {
+      this->_pending.reserve(40);
+   }
+   void form_stub_use_info_builder::add_outbound_reference(form_stub* to_stub, use_info_entry::flags_t flags) {
+      this->_pending.emplace_back(to_stub, flags);
+   }
+   void form_stub_use_info_builder::add_outbound_reference(uint32_t toFormID, use_info_entry::flags_t flags) {
+      this->_pending.emplace_back(toFormID, flags);
+   }
+   void form_stub_use_info_builder::commit() {
+      for (auto& entry : this->_pending) {
+         if (auto* to_stub = entry.target_stub)
+            this->_stub._add_one_way_outbound_reference(to_stub, entry.flags);
+         else
+            this->_stub._add_one_way_outbound_reference(entry.target_id, entry.flags);
+      }
+      this->_pending.clear();
+   }
+   //
+   form_stub_use_info_builder* form_stub_use_info_builder::spawn_subordinate() const noexcept {
+      auto sub = new form_stub_use_info_builder(this->_stub);
+      sub->_is_final_file     = this->_is_final_file;
+      sub->_last_record_flags = this->_last_record_flags;
+      sub->is_partial_record  = this->is_partial_record;
+      return sub;
+   }
+   //
+   void form_stub_use_info_builder::clear_all_prior_use_info() const noexcept {
+      use_info_entry parent_entry;
+      //
+      auto& stub = this->_stub;
+      for (auto& pair : stub.outbound) {
+         auto& entry = pair.second;
+         if (entry.flags & use_info_entry::flag::i_am_child_of)
+            parent_entry = entry;
+      }
+      stub.outbound.clear();
+      if (auto* parent = parent_entry.other) {
+         //
+         // Do not clear parent/child relationships.
+         //
+         parent_entry.refcount = 1;
+         stub.outbound[parent->formID] = parent_entry;
+      }
+   }
+}
