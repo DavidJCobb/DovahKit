173ebf4e073a29d4a3236c01b3131e24b214983c
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2021-03-08 16:22:02 +0100

    The frontend core can now find loose files when asked to load assets
    The dovah::bsa_archived_file class can now be constructed from cobb::generic_buffer instances, which gives us an appropriately lazy way to use the same interfaces for dealing with BSA-packed and loose files within the frontend
    
    DovahKitCore::lookup_game_asset is now capable of finding and returning loose files.

--- native/src/DovahKit/dovah/files/bsa/bsa_archived_file.h
+++ native/src/DovahKit/dovah/files/bsa/bsa_archived_file.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #include "../../../helpers/memory.h"
 
 namespace dovah {
@@ -14,14 +15,24 @@ namespace dovah {
             lz4_error,
          };
       protected:
-         cobb::generic_buffer owned;
+         cobb::generic_buffer owned; // used for files that needed to be decompressed and, theoretically, loose files
          struct {
             const void* data = nullptr;
             size_t size = 0;
-         } shared;
+         } shared; // used for files that could be pulled directly out of a memory-mapped BSA file
          error_code error = error_code::none;
          //
       public:
+         bsa_archived_file() {};
+         //
+         bsa_archived_file(const cobb::generic_buffer& buf) : owned(buf) {};
+         bsa_archived_file(cobb::generic_buffer&& buf) : owned(buf) {};
+         //
+         bsa_archived_file(const void* shared_data, size_t s) {
+            this->shared.data = shared_data;
+            this->shared.size = s;
+         }
+         
          inline const void* data() const noexcept {
             const void* d = this->owned.data();
             if (!d)
@@ -35,7 +46,8 @@ namespace dovah {
          }
          inline error_code get_error() const noexcept { return this->error; }
          inline bool has_error() const noexcept { return this->error != error_code::none; }
-         //
+         inline bool is_shared() const noexcept { return !this->owned.data(); }
+         
          void read(size_t offset, void* value, size_t size) const noexcept {
             if (!this->data())
                return;
