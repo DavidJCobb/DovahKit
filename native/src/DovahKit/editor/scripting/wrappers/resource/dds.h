eaae9bad30339b3493eb579fccf6683ed67ed4ef
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2021-05-27 17:30:09 +0200

    Work on scripting: work on LMR subresources
    Updated our internal documentation for Lua-managed resources, to explain why we handle subresources the way we do -- or rather, the way we don't.
    
    Added Lua wrapper-part-types for DDS subresources.

--- native/src/DovahKit/editor/scripting/wrappers/resource/dds.h
+++ native/src/DovahKit/editor/scripting/wrappers/resource/dds.h
@@ -1,6 +1,25 @@
 #pragma once
 #include "../../wrapper.h"
 
+namespace editor_script::wrapper_part_types {
+   inline constexpr cobb::eight_cc resource_dds_image              = "DDSImage";
+   inline constexpr cobb::eight_cc resource_dds_cubemap            = "DDSCubRt";
+   inline constexpr cobb::eight_cc resource_dds_cubemap_face_x_pos = "DDSCubX+";
+   inline constexpr cobb::eight_cc resource_dds_cubemap_face_x_neg = "DDSCubX-";
+   inline constexpr cobb::eight_cc resource_dds_cubemap_face_y_pos = "DDSCubY+";
+   inline constexpr cobb::eight_cc resource_dds_cubemap_face_y_neg = "DDSCubY-";
+   inline constexpr cobb::eight_cc resource_dds_cubemap_face_z_pos = "DDSCubZ+";
+   inline constexpr cobb::eight_cc resource_dds_cubemap_face_z_neg = "DDSCubZ-";
+   inline constexpr cobb::eight_cc resource_dds_image_mipmap_level = "DDSMipLv";
+   //
+   // dds.images[n]                               | DDSImage[n]
+   // dds.images[n].cubemap_faces                 | DDSImage[n]/DDSCubRt
+   // dds.images[n].cubemap_faces.x_pos           | DDSImage[n]/DDSCubX+
+   // dds.images[m].mipmap[n]                     | DDSImage[m]/DDSMipLv[n]
+   // dds.images[m].cubemap_faces.x_pos.mipmap[n] | DDSImage[m]/DDSCubX+/DDSMipLv[n]
+   //
+}
+
 namespace editor_script::wrappers::resource {
    struct dds : public wrapper_metatable {
       static constexpr const char* superclass_key = metatable_key;
