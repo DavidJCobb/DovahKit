08d417ceae28f1090b4e420b2f0f5e6bdfad6145
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2021-07-29 18:14:50 +0200

    Dovahscript: removed the widget metatable override property
    Removed "metatable override" functionality from the new script engine, as this functionality can't easily be used from the worker thread: dynamic QObject property accessors are reentrant but not thread-safe, and we can't be sure under what conditions Qt itself might try to get or set properties (though we know they have a defined prefix for Qt-internal properties). If we want widgets of a particular type to use a particular metatable, we'll just have to subclass those widgets.
    
    The override functionality was mainly useful so that horizontal rules could be bare QFrames without every QFrame being locked to the "horizontal/vertical rule" class in Lua. Again, though, it's simpler and easier to subclass them.

--- native/src/DovahKit/dovahscript/widget_overrides.cpp
+++ native/src/DovahKit/dovahscript/widget_overrides.cpp
@@ -1,29 +1,22 @@
 #include "widget_overrides.h"
+#include "core/verify_threading.h"
 
 namespace {
-   static constexpr const char* property_for_parent_override    = "dovahscript: widget forced parent";
-   static constexpr const char* property_for_metatable_override = "dovahscript: force widget metatable key to";
+   static constexpr const char* property_for_parent_override = "dovahscript: widget forced parent";
 }
 
 namespace dovahscript {
    [[nodiscard]] extern QWidget* get_widget_forced_parent(QWidget* widget) {
+      core::require_client_thread();
+      //
       auto data = widget->property(property_for_parent_override);
       if (data.isValid())
          return (QWidget*)data.value<QObject*>();
       return nullptr;
    }
    extern void set_widget_forced_parent(QWidget* subject, QWidget* parent) {
+      core::require_client_thread();
+      //
       subject->setProperty(property_for_parent_override, QVariant::fromValue<QObject*>(parent));
-   }
-
-   [[nodiscard]] extern const char* get_widget_metatable_override(QWidget* widget) {
-      auto data = widget->property(property_for_metatable_override);
-      if (data.isValid())
-         return (const char*)data.value<void*>();
-      return nullptr;
-   }
-   extern void set_widget_metatable_override(QWidget* widget, const char* metatable_key) {
-      assert(widget && metatable_key);
-      widget->setProperty(property_for_metatable_override, QVariant::fromValue<void*>((void*)metatable_key));
    }
 }
--- native/src/DovahKit/dovahscript/widget_overrides.h
+++ native/src/DovahKit/dovahscript/widget_overrides.h
@@ -5,7 +5,4 @@
 namespace dovahscript {
    [[nodiscard]] extern QWidget* get_widget_forced_parent(QWidget* widget);
    extern void set_widget_forced_parent(QWidget* subject, QWidget* parent);
-
-   [[nodiscard]] extern const char* get_widget_metatable_override(QWidget* widget);
-   extern void set_widget_metatable_override(QWidget* widget, const char* metatable_key);
 }
