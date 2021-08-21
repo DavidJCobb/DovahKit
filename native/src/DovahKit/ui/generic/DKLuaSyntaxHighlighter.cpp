0e93e7dff27cef135e92b4421d10d939585341fe
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2021-08-21 15:59:00 +0200

    DKLuaSyntaxHighlighter improvements
    The syntax highlighter now understands what Lua calls "long brackets" and their levels; accordingly, we now handle block-comments and block-strings properly, where previously, the former were mishandled if not on level 0 while the latter were entirely unsupported.
    
    We now highlight labels, numbers, and operators.
    
    We now highlight the content of unterminated non-block strings if their first line ending was backslash-escaped (i.e. `"\"foo \\\n"`).

--- native/src/DovahKit/ui/generic/DKLuaSyntaxHighlighter.cpp
+++ native/src/DovahKit/ui/generic/DKLuaSyntaxHighlighter.cpp
@@ -14,6 +14,7 @@ namespace {
       QString("elseif"),
       QString("end"),
       QString("false"),
+      QString("goto"),
       QString("if"),
       QString("in"),
       QString("local"),
@@ -28,12 +29,135 @@ namespace {
       QString("while"),
    };
 
+   const std::array operators = {
+      QString("="),
+      QString("+="),
+      QString("-="),
+      QString("*="),
+      QString("/="),
+      QString("~="),
+      QString("+"),
+      QString("-"),
+      QString("*"),
+      QString("/"),
+      QString("&"),
+      QString("|"),
+      QString("~"),
+      QString("^"),
+      QString("<<"),
+      QString(">>"),
+      // Compare:
+      QString("=="),
+      QString("<"),
+      QString(">"),
+      QString("<="),
+      QString(">="),
+      // Syntax:
+      QString("#"),   // length
+      QString("("),
+      QString(")"),
+      QString("["),
+      QString("]"),
+      QString(","),
+      QString("."),   // member access
+      QString(";"),
+      QString(":"),   // selfcall
+      QString(".."),  // concatenate
+      QString("..."), // pack
+   };
+
    static bool is_keyword_boundary(QChar c) {
       if (c == '_')
          return false;
       if (c.isLetterOrNumber())
          return false;
       return true;
+   }
+
+   static bool is_name_char(QChar c) {
+      // Per Lua 5.4 manual section 3.1: names can be Latin letters, Arabic-indic digits, and underscores, 
+      // not beginning with a digit and not being a reserved word
+      if (c == '_')
+         return true;
+      if (c.isLetterOrNumber())
+         return true;
+      return false;
+   }
+
+   //
+   // Given a Lua long bracket, e.g. `[[` or `[===[`, extracts the level (number of equal signs) 
+   // and the total length of the token. If there is no long bracket, the level is set to -1.
+   // 
+   // The first character in the QStringRef should be the first `[`.
+   //
+   static void extract_long_bracket(const QStringRef& view, int& level, int& token_length) {
+      level        = -1;
+      token_length =  0;
+      if (view[0] != '[')
+         return;
+      bool match = true;
+      bool ended = false;
+      auto size  = view.size();
+      int  i     = 1;
+      for (; i < size; ++i) {
+         QChar c = view[i];
+         if (c != '=') {
+            ended = c == '[';
+            match = ended;
+            break;
+         }
+      }
+      if (match && ended) {
+         token_length = i + 1;
+         level        = i - 1;
+      }
+   }
+
+   static bool extract_long_bracket_close(const QStringRef& view, int level, int& token_length) {
+      token_length = 0;
+      if (view[0] != ']')
+         return false;
+      bool match = true;
+      bool ended = false;
+      auto size  = view.size();
+      int  i     = 1;
+      for (; i < size; ++i) {
+         QChar c = view[i];
+         if (c != '=') {
+            ended = c == ']';
+            match = ended;
+            break;
+         }
+      }
+      if (match && ended) {
+         if (i - 1 == level) {
+            token_length = i + 1;
+            return true;
+         }
+      }
+      return false;
+   }
+
+   static const QString* extract_keyword(const QStringRef& view) {
+      auto size = view.size();
+      for (auto& keyword : keywords) {
+         if (view.startsWith(keyword)) {
+            auto kw_size = keyword.size();
+            bool bounded = true;
+            if (size > kw_size)
+               bounded = is_keyword_boundary(view[kw_size]);
+            if (bounded)
+               return &keyword;
+         }
+      }
+      return nullptr;
+   }
+
+   static const QString* extract_operator(const QStringRef& view) {
+      for (auto& keyword : operators)
+         if (view.startsWith(keyword))
+            return &keyword;
+      return nullptr;
    }
 }
 
@@ -44,109 +168,196 @@ DKLuaSyntaxHighlighter::DKLuaSyntaxHighlighter(QTextDocument* document) : QSynta
    this->formats.comment.line.setForeground(QColor::fromRgb(48, 192, 8));
    this->formats.keyword.setFontWeight(QFont::Weight::Bold);
    this->formats.keyword.setForeground(QColor::fromRgb(0, 16, 255));
-   this->formats.string.setForeground(QColor::fromRgb(140, 140, 140));
+   this->formats.label.setFontWeight(QFont::Weight::Bold);
+   this->formats.label.setForeground(QColor::fromRgb(128, 128, 0));
+   this->formats.number.setForeground(QColor::fromRgb(200, 100, 0));
+   this->formats.op.setForeground(QColor::fromRgb(0, 0, 128));
+   this->formats.op.setFontWeight(QFont::Weight::Bold);
+   this->formats.string.simple.setForeground(QColor::fromRgb(140, 140, 140));
+   this->formats.string.block.setForeground(QColor::fromRgb(160, 0, 80));
 }
 void DKLuaSyntaxHighlighter::highlightBlock(const QString& text) {
-   int state = this->previousBlockState();
-   int start = 0;
+   auto state   = BlockState(this->previousBlockState());
+   auto initial = state;
+   int  start   = 0;
+   const auto size = text.size();
    //
-   if (state == BlockState::None) {
+   if (state.is_none()) {
+      //
+      // Catch keywords at the start of the block:
+      //
       for (auto& keyword : keywords) {
-         if (text == keyword) {
-            this->setFormat(0, text.size(), this->formats.keyword);
-            return;
+         if (text.startsWith(keyword)) {
+            auto kw_size = keyword.size();
+            if (size > kw_size) {
+               if (!is_keyword_boundary(text[kw_size]))
+                  continue;
+            }
+            this->setFormat(0, kw_size, this->formats.keyword);
+            break;
          }
       }
    }
    //
-   for (int i = 0; i < text.length(); ++i) {
-      switch (state) {
-         case BlockState::Comment_Block:
-            if (text.mid(i, 4) == "]]--") {
-               state = BlockState::None;
-               this->setFormat(start, i - start + 4, this->formats.comment.block);
-               //
-               // Skip ahead by the size of the delimiter (minus one because the for-loop will 
-               // also increment us), so that the end of a block comment isn't also treated as 
-               // the start of a line comment.
-               //
-               i += 4 - 1;
-            }
-            break;
-         case BlockState::Comment_Line:
-            if (text[i] == '\n') {
-               state = BlockState::None;
-               this->setFormat(start, i - start, this->formats.comment.line);
-            }
-            break;
-         case BlockState::String_Double:
-            if (text[i] == '"') {
-               state = BlockState::None;
-               this->setFormat(start, i - start + 1, this->formats.string);
-            }
-            break;
-         case BlockState::String_Single:
-            if (text[i] == '\'') {
-               state = BlockState::None;
-               this->setFormat(start, i - start + 1, this->formats.string);
-            }
-            break;
-         default:
-            if (text[i] == '"') {
-               state = BlockState::String_Double;
+   bool backslash = false;
+   for (int i = 0; i < size; ++i) {
+      QChar c  = text[i];
+      auto  cd = text.mid(i, 2);
+      if (state.is_none()) {
+         if (c == '"' || c == '\'') {
+            start = i;
+            state = BlockState(TokenType::String_Simple, c.toLatin1());
+            continue;
+         }
+         if (c == '[') {
+            int length =  0;
+            int level  = -1;
+            extract_long_bracket(QStringRef(&text, i, size - i), level, length);
+            if (level > BlockState::max_supported_param)
+               level = -1;
+            if (level >= 0) {
                start = i;
-               break;
+               state = BlockState(TokenType::String_Block, level);
+               continue;
             }
-            if (text[i] == '\'') {
-               state = BlockState::String_Single;
-               start = i;
-               break;
+         }
+         if (cd == "--") {
+            //
+            // Handle comments:
+            //
+            int   length =  0;
+            int   level  = -1;
+            QChar e      = (i + 2) < size ? text.at(i + 2) : '\0';
+            if (e == '[') {
+               extract_long_bracket(QStringRef(&text, i + 2, size - i - 2), level, length);
+               if (level > BlockState::max_supported_param)
+                  level = -1;
             }
-            if (text.mid(i, 4) == "--[[") {
-               state = BlockState::Comment_Block;
-               start = i;
-               break;
+            start = i;
+            if (level >= 0) {
+               state = BlockState(TokenType::Comment_Block, level);
+            } else {
+               state = BlockState(TokenType::Comment_Line);
             }
-            if (text.mid(i, 2) == "--") {
-               state = BlockState::Comment_Line;
-               start = i;
-               break;
-            }
-            if (is_keyword_boundary(text[i])) {
-               // search backward for a keyword
-               QStringRef view = QStringRef(&text, 0, i);
-               for (auto& keyword : keywords) {
-                  if (view.endsWith(keyword)) {
-                     auto size    = keyword.size();
-                     bool bounded = true;
-                     if (size > i) {
-                        bounded = is_keyword_boundary(text[i - size]);
-                     }
-                     if (bounded) {
-                        this->setFormat(i - size, size, this->formats.keyword);
-                     }
+            i += length;
+            continue;
+         }
+         if (cd == "::") {
+            //
+            // Handle labels i.e. `::label_name::`
+            //
+            int  l = text.length();
+            int  j = i + 2;
+            bool match = true;
+            bool ended = false;
+            for (; j < l; ++j) {
+               auto c = text[j];
+               if (!is_name_char(c)) {
+                  if (c == ':' && j + 1 < l && text[j + 1] == ':') { // end-delimiter
+                     ended = true;
+                     break;
                   }
+                  match = false;
+                  break;
+               }
+            }
+            if (match && ended) {
+               this->setFormat(i, j + 2 - i, this->formats.label);
+               i = j + 2 - 1; // subtract 1 because we're in a for-loop, which will increment
+               continue;
+            }
+         }
+         //
+         auto next = QStringRef(&text, i, size - i);
+         if (i && is_keyword_boundary(text[i - 1]) && !is_keyword_boundary(c)) {
+            auto  view = QStringRef(&text, i, size - i);
+            auto* keyword = extract_keyword(view);
+            if (keyword) {
+               auto length = keyword->size();
+               this->setFormat(i, length, this->formats.keyword);
+               i += length - 1;
+               continue;
+            }
+         }
+         if (i == 0 || is_keyword_boundary(text[i - 1])) {
+            if (c.isDigit()) {
+               int j = i;
+               for (; j < size; ++j) {
+                  if (!text[j].isDigit())
+                     break;
+               }
+               int length = j - i;
+               this->setFormat(i, length, this->formats.number);
+               i += length - 1;
+               continue;
+            }
+         }
+         if (auto* keyword = extract_operator(next)) {
+            auto length = keyword->size();
+            this->setFormat(i, length, this->formats.op);
+            i += length - 1;
+            continue;
+         }
+         continue;
+      }
+      //
+      // We're in some sort of special token.
+      //
+      auto next   = QStringRef(&text, i, size - i);
+      int  length = 0;
+      switch (state.code()) {
+         case TokenType::String_Simple:
+            backslash = (c == '\\');
+            if (c == QChar::fromLatin1(state.param())) {
+               this->setFormat(start, i - start + 1, this->formats.string.simple);
+               state.clear();
+            }
+            break;
+         case TokenType::String_Block:
+            if (extract_long_bracket_close(next, state.param(), length)) {
+               this->setFormat(start, i - start + length, this->formats.string.block);
+               state.clear();
+               i += length - 1;
+            }
+            break;
+         case TokenType::Comment_Block:
+            if (extract_long_bracket_close(next, state.param(), length)) {
+               if (text.mid(i + length, 2) == "--") {
+                  this->setFormat(start, i - start + length + 2, this->formats.comment.block);
+                  state.clear();
+                  i += length + 2 - 1;
                }
             }
             break;
       }
    }
-   switch (state) {
-      case BlockState::Comment_Block:
+   //
+   // Reached the end of the block. We typically apply formatting to tokens when we've 
+   // encountered their ends, so if we're inside of any tokens now, we need to apply 
+   // formatting at this time.
+   // 
+   // Qt's documentation doesn't say this outright, but the end of a text block is always 
+   // either the end of a line or the end of the document entirely. As such, any states 
+   // that should not persist past the end of the line must be exited here.
+   //
+   switch (state.code()) {
+      case TokenType::Comment_Block:
          this->setFormat(start, text.size() - start, this->formats.comment.block);
          break;
-      case BlockState::Comment_Line:
+      case TokenType::Comment_Line:
          this->setFormat(start, text.size() - start, this->formats.comment.line);
-         //
-         // Qt's documentation is not at all clear about this, but a "block" is never more than one 
-         // line; ergo any states that must terminate at the end of a line should not be stored.
-         //
-         [[fallthrough]];
-      case BlockState::String_Double:
-         [[fallthrough]];
-      case BlockState::String_Single:
-         state = BlockState::None;
+         state.clear();
+         break;
+      case TokenType::String_Simple:
+         if (backslash || initial.code() == TokenType::String_Simple) {
+            this->setFormat(start, text.size() - start, this->formats.string.simple);
+         } else {
+            state.clear();
+         }
+         break;
+      case TokenType::String_Block:
+         this->setFormat(start, text.size() - start, this->formats.string.block);
          break;
    }
-   setCurrentBlockState(state);
+   setCurrentBlockState(state.to_int());
 }
--- native/src/DovahKit/ui/generic/DKLuaSyntaxHighlighter.h
+++ native/src/DovahKit/ui/generic/DKLuaSyntaxHighlighter.h
@@ -7,14 +7,40 @@ class DKLuaSyntaxHighlighter : public QSyntaxHighlighter {
    public:
        DKLuaSyntaxHighlighter(QTextDocument* parent = nullptr);
 
-       struct BlockState {
-          enum {
-             None = -1,
-             Comment_Line,
-             Comment_Block,
-             String_Double,
-             String_Single,
-          };
+       enum class TokenType {
+          None,
+          Comment_Line,
+          Comment_Block,
+          String_Simple,
+          String_Block,
+       };
+
+       class BlockState {
+          protected:
+             int value = 0;
+
+             static constexpr const int code_bitcount = 8;
+             static constexpr const int code_mask     = (1 << code_bitcount) - 1;
+             static_assert(code_bitcount < sizeof(int) * 8);
+
+          public:
+            BlockState() {}
+            BlockState(int i) : value(i) {}
+            BlockState(TokenType t, int param = 0) : value((int)t | (param << code_bitcount)) {}
+
+            static constexpr const int param_bits          = sizeof(int) * 8 - code_bitcount;
+            static constexpr const int max_supported_param = (1 << param_bits) - 1;
+
+            inline TokenType code() const noexcept { return (TokenType)(this->value & code_mask); }
+            inline int param() const noexcept { return this->value >> code_bitcount; }
+
+            inline bool is_none() const noexcept { return (this->value == -1) || code() == TokenType::None; }
+
+            inline void clear() noexcept {
+               this->value = 0;
+            }
+
+            inline int to_int() const noexcept { return this->value; }
        };
 
    protected:
@@ -27,6 +53,12 @@ class DKLuaSyntaxHighlighter : public QSyntaxHighlighter {
              QTextCharFormat block;
           } comment;
           QTextCharFormat keyword;
-          QTextCharFormat string;
+          QTextCharFormat label;
+          QTextCharFormat number;
+          QTextCharFormat op;
+          struct {
+             QTextCharFormat simple;
+             QTextCharFormat block;
+          } string;
        } formats;
 };
