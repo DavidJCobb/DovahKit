9b1961176abe97fcc3a62f7700bb6e2efef6cd64
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2022-10-27 05:19:04 +0200

    Update NiTransform.cpp
    Code comments here displayed a wrong Euler matrix (righthanded instead of lefthanded). Easier to just defer to the comments I wrote elsewhere.

--- native/src/DovahKit/nif/types/NiTransform.cpp
+++ native/src/DovahKit/nif/types/NiTransform.cpp
@@ -40,23 +40,10 @@ namespace nifDK {
          // then combine them back into a new matrix.
          //
          {
-            constexpr float EPSILON = 0.000001;
             //
-            // Sadly, there's no quick shortcut. Given separate matrices for X, Y, and Z, you can 
-            // swap handedness by taking the transpose of each... but that doesn't work once they 
-            // have been multiplied together.
-            // 
-            // Righthanded XYZ matrices look like this:
-            // 
-            //    [   cosY*cosZ                    ,   cosY*sinZ                    ,   sinY        , 
-            //        sinX*sinY*cosZ + cosX*sinZ   ,  -sinX*sinY*sinZ + cosX*cosZ   ,  -sinX*cosY   , 
-            //       -cosX*sinY*cosZ + sinX*sinZ   ,   cosX*sinY*sinZ + sinX*cosZ   ,   cosX*cosY   ]
-            // 
-            // So the first thing we should try is  taking that upper-right corner element, sinY, 
-            // and running asin on it. That'll immediately  get us the Y-angle. Next, take a look 
-            // at the top-left, top-center, center-right,  and bottom-right elements: they're all 
-            // multiplied by the cosine of Y. If the  cosine of Y is non-zero, then we can use it 
-            // to decode those elements.
+            // For an explanation, see the `/cobb/rotation.cpp` file.
+            //
+            constexpr float EPSILON = 0.000001;
             //
             float x;
             float y = asin(-out[2][0]); // col 2, row 0 == sin(y)
@@ -67,31 +54,14 @@ namespace nifDK {
             //
             float cosY = cos(y);
             if (fabs(cosY) > EPSILON) {
-               //
-               // We lucked out and the cosine of Y is  indeed non-zero. That means we can decode 
-               // those other two-term elements. Let's start with the ones that'll give us X: the 
-               // center-right and bottom-right elements.  They were (-sinX*cosY) and (cosX*cosY) 
-               // respectively.
-               // 
-               // If we can isolate sinX and cosX, then we  can pass those into atan2 (as X and Y 
-               // arguments, respectively) to compute X itself.
-               //
                u = out[2][2] / cosY; // u == cosX
                v = out[2][1] / cosY; // v == sinX
                x = atan2(v, u);
-               //
-               // And with the other two elements,  the top-left and top-center, we can get Z the 
-               // same way.
                //
                u = out[0][0] / cosY; // u == cosZ
                v = out[1][0] / cosY; // v == sinZ
                z = atan2(v, u);
             } else {
-               //
-               // Can't deduce X and Z from Y. Just assume Z is zero and dump everything into X. 
-               // If Z is zero,  then cos(Z) == 1 and sin(Z) == 0;  we can use this to eliminate 
-               // terms from the combined matrix.
-               //
                z = 0;
                u = out[1][1]; // -sinX*sinY*sinZ + cosX*cosZ == -sinX*sinY*0 + cosX*1 == cosX
                v = -out[0][1]; //  cosX*sinY*sinZ + sinX*cosZ ==  cosX*sinY*0 + sinX*1 == sinX
@@ -116,9 +86,7 @@ namespace nifDK {
       //      glm::vec3(this->scale)
       //   ) * glm::mat4(this->rotation);
       //
-      // The above code would work if the rotation were lefthanded. If we do the math by hand, 
-      // we can take a few shortcuts since we know  what our data should look like, and we can 
-      // correct the handedness as well.
+      // The above code would work if NIF angles were righthanded.
       //
       // glm::scale just multiplies the first three columns of the first argument by the three 
       // scalars (one per axis) supplied in the second argument. NiTransform only does uniform 
