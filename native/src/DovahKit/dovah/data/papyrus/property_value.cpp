220826a2b557e59625bf204fd65bc5198e9bcad7
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2024-01-04 15:35:48 +0100

    Removing unused code

--- native/src/DovahKit/dovah/data/papyrus/property_value.cpp
+++
@@ -1,9 +0,0 @@
-#include "./property_value.h"
-#include "dovah/forms/components/papyrus/property_object_value.h"
-
-namespace dovah::papyrus {
-   property_object_value::property_object_value(const loaded_forms::components::papyrus::property_object_value& src) {
-      this->form     = src.form.get_form_stub();
-      this->alias_id = src.alias_id;
-   }
-}
