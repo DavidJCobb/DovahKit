7598f9fbb2d64941f9b47f304ef749e1357ca26c
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2026-02-15 19:21:45 -0500

    Create is_std_pair.h

---
+++ native/src/DovahKit/helpers/type_traits/is_std_pair.h
@@ -0,0 +1,27 @@
+/*
+
+This file is provided under the Creative Commons 0 License.
+License: <https://creativecommons.org/publicdomain/zero/1.0/legalcode>
+Summary: <https://creativecommons.org/publicdomain/zero/1.0/>
+
+One-line summary: This file is public domain or the closest legal equivalent.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+#pragma once
+#include <type_traits>
+#include <utility>
+
+namespace cobb {
+   template<typename T> concept is_std_pair = requires(T x) {
+      typename T::first_type;
+      typename T::second_type;
+      requires std::is_same_v<T, std::pair<typename T::first_type, typename T::second_type>>;
+   };
+}
